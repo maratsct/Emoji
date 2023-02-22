bc08ad66da38b9617d7a206c510f48907819ad18
Marat <hambikyan@gmail.com>
2023-02-22 18:45:45 +0400

    Relesae 1.2.2

--- app/src/main/java/com/sceyt/axempji/MainActivity.kt
+++ app/src/main/java/com/sceyt/axempji/MainActivity.kt
@@ -2,10 +2,18 @@ package com.sceyt.axempji
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.emojiview.emojiview.AXEmojiManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AXEmojiManager.install(application)
+
+        findViewById<TextView>(R.id.tv).setOnClickListener {
+            BottomSheetEmojisFragment({}).show(supportFragmentManager,"dsf")
+        }
     }
 }
